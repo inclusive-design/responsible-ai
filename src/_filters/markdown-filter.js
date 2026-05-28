42d2c1c4fed6c262a65f61cbb6229c421253ca98
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-05-28 11:35:01 -0300

    feat: add autolinks for Risk and Opportunity references
    Resolves #25

--- src/_filters/markdown-filter.js
+++ src/_filters/markdown-filter.js
@@ -5,6 +5,17 @@ import MarkdownIt from 'markdown-it';
  * @returns {string} - Rendered HTML.
  */
 export default function markdownFilter(value) {
+	/**
+	 * Automatically link risk index references to markdown links to the specified risk.
+	 * @param {string} match - The complete matched substring from the regular expression.
+	 * @returns {string} A markdown link in the format `[text](../text)` with lowercased path.
+	 */
+	function riskIndexToMarkdownLink(match) {
+		return `[${match}](../${match.toLowerCase()})`;
+	}
+
+	const output = value.replaceAll(/[RO]\d+/gu, riskIndexToMarkdownLink);
+
 	const md = new MarkdownIt();
-	return md.render(value);
+	return md.render(output);
 }
