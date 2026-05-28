883c6168195cadb1483dd16faa41ce381353ff36
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-05-28 15:23:11 -0300

    fix: resolve filter issue (#32)

--- src/assets/scripts/filter.js
+++ src/assets/scripts/filter.js
@@ -1,10 +1,6 @@
 /* eslint-disable import-x/no-unassigned-import */
 import './filter-container.js';
 
-window.addEventListener('load', () => {
-	renderFilterTags();
-});
-
 const filters = document.querySelector('#filters');
 const filterOptions = filters.querySelectorAll('input[type=\'checkbox\']');
 for (const checkbox of filterOptions) {
@@ -13,22 +9,33 @@ for (const checkbox of filterOptions) {
 	});
 }
 
+globalThis.setTimeout(() => {
+	renderFilterTags();
+}, 0);
+
+document.addEventListener('click', (event) => {
+	if (event.target.closest('#filter-tags button')) {
+		const button = event.target.closest('#filter-tags button');
+		const checkbox = filters.querySelector(`#${button.dataset.id}`);
+		checkbox.click();
+	}
+});
+
 const renderFilterTags = () => {
 	const filterTags = document.querySelector('#filter-tags');
 	filterTags.innerHTML = '';
-	const checkedFilterOptions = filters.querySelectorAll('input[type=\'checkbox\']:checked');
+	const checkedFilterOptions = filters.querySelectorAll('input:checked');
 	if (checkedFilterOptions.length > 0) {
 		const filterApplied = document.querySelector('#filter-applied');
 		filterApplied.style.display = 'block';
 		for (const option of checkedFilterOptions) {
-			const checkbox = filters.querySelector(`[for='${option.id}']`);
+			const checkboxLabel = filters.querySelector(`[for='${option.id}']`);
 			const template = document.querySelector('#clear-button');
 			const clone = document.importNode(template.content, true);
-			clone.addEventListener('click', () => {
-				checkbox.click();
-			});
+			const button = clone.querySelector('button');
+			button.dataset.id = option.id;
 			const label = clone.querySelector('.label');
-			label.innerHTML = checkbox.innerHTML;
+			label.innerHTML = checkboxLabel.innerHTML;
 			filterTags.append(clone);
 		}
 	} else {
