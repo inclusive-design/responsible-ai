1a501caf5c654adf9ff2f670445e23e444f0d788
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-05-28 11:51:11 -0300

    feat: add autolinks for Risk and Opportunity references (resolves #25) (#26)
    * feat: add autolinks for Risk and Opportunity references
    * feat: handle both types with localized URLs
    * fix: improve link fragment accuracy

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ export default defineConfig([
 		extends: [eslintConfigInclusiveDesign],
 		rules: {
 			camelcase: ['error', { properties: 'never' }],
+			'require-unicode-regexp': ['error', { requireFlag: 'u' }],
 		},
 	},
 	{
--- src/_filters/link-fragment-filter.js
+++ src/_filters/link-fragment-filter.js
@@ -4,7 +4,7 @@
  */
 export default function linkFragmentFilter(value) {
 	const words = value.split(' ');
-	const start = words.slice(0, 3).join(' ');
-	const end = words.slice(-3).join(' ');
+	const start = words.slice(0, 4).join(' ');
+	const end = words.slice(-4).join(' ');
 	return `#:~:text=${encodeURIComponent(start).replace('-', '%2D')},${encodeURIComponent(end).replace('-', '%2D')}`;
 }
