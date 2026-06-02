737e7434b4f34d826d70c5cf39236ceb2d7bdb8f
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-06-02 13:20:45 -0300

    fix: remove unneccesary config values

--- sugarcube.config.js
+++ sugarcube.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from '@sugarcube-sh/cli';
 
 export default defineConfig({
 	resolver: 'src/design-tokens/tokens.resolver.json',
-	variables: {
-		path: '_site/assets/styles/variables.gen.css',
-	},
-	utilities: {
-		path: '_site/assets/styles/utilities.gen.css',
-	},
 	components: 'src/assets/styles/components/ui',
 	cube: 'src/assets/styles',
 });
